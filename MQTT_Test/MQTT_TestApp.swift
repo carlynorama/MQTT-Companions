00a80340b38dc1996972f7366674e161a80fd2df
carlynorama <carlynorama@gmail.com>
2020-09-30 08:43:08 -0700

    Initial Commit

---
+++ MQTT_Test/MQTT_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTT_TestApp.swift
+//  MQTT_Test
+//
+//  Created by Carlyn Maw on 9/30/20.
+//
+
+import SwiftUI
+
+@main
+struct MQTT_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
